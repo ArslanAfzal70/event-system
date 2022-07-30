7736424f6e751c2e72497938b6355fb4a941f504
Arslan Afzal <arslanafzal794@gmail.com>
2022-07-31 02:02:55 +0500

    Added sidebar and top menu in dashboard page
    Added sidebar and top menu in dashboard page

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,53 +1,103 @@
 <template>
     <div>
-        <nav class="navbar navbar-expand-lg bg-light">
+        <!-- ---------------- NAVBAR ----------------- -->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-purple">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#">Navbar</a>
+                <!-- ----------- OFFCANVAS TRIGGER ----------- -->
+                <button class="navbar-toggler me-2" type="button" data-bs-toggle="offcanvas"
+                    data-bs-target="#offcanvasExample" aria-controls="offcanvasExample">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <a class="navbar-brand fw-bold me-auto" href="#">
+                    <font-awesome-icon icon="fa-solid fa-calendar-days" />
+                    <span class="ms-2">Event Manager</span>
+                </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                     data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                     aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Link</a>
-                        </li>
+                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
                                 aria-expanded="false">
-                                Dropdown
+                                <font-awesome-icon icon="fa-solid fa-user" />
+                                <span class="ms-2">{{ user.name }}</span>
                             </a>
-                            <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="#">Action</a></li>
-                                <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <ul class="dropdown-menu dropdown-menu-end">
+                                <li><a class="dropdown-item" href="#">Profile</a></li>
                                 <li>
                                     <hr class="dropdown-divider">
                                 </li>
-                                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                <li><a class="dropdown-item" href="#">Logout</a></li>
                             </ul>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link disabled">Disabled</a>
-                        </li>
                     </ul>
-                    <form class="d-flex" role="search">
-                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                        <button class="btn btn-outline-success" type="submit">Search</button>
-                    </form>
+
                 </div>
             </div>
         </nav>
+
+        <!-- -------------- OFFCAVANAS --------------- -->
+
+        <div class="offcanvas offcanvas-start sidebar-nav bg-purple" tabindex="-1" id="offcanvasExample"
+            aria-labelledby="offcanvasExampleLabel">
+            <div class="offcanvas-body">
+
+            </div>
+        </div>
+
+
     </div>
 </template>
 <script>
 export default {
     name: 'DashbaordView',
+    data() {
+        return {
+            user: {}
+        }
+    },
+    mounted() {
+        this.user = JSON.parse(localStorage.getItem('user'));
+    },
 }
 </script>
-<style lang="">
-    
+<style lang="scss">
+$dark-purpule : #32213A;
+$yellow : #F4BF01;
+$sidebarWdith : 300px;
+
+input,
+button {
+    outline: none !important;
+    border: none !important;
+}
+
+button:focus {
+    box-shadow: none;
+}
+
+.bg-purple {
+    background-color: $dark-purpule;
+}
+
+.sidebar-nav {
+    width: $sidebarWdith  !important;
+}
+
+@media (min-width:992px) {
+    body {
+        overflow: auto !important;
+    }
+
+    .sidebar-nav {
+        transform: none !important;
+        visibility: visible !important;
+        top: 56px !important;
+        // height: calc(100% - 56px) !important;
+    }
+}
 </style>
