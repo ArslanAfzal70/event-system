6a395c6507820576222038775348937dab139718
Arslan Afzal <arslanafzal794@gmail.com>
2022-08-01 02:34:50 +0500

    Created migration, model and seeder for events
    Created migration, model and seeder for events

---
+++ backend/database/migrations/2022_07_31_210735_create_events_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('description');
+            $table->dateTime('date_start');
+            $table->dateTime('date_end');
+            $table->decimal('silver_price', 9, 2)->default(0.00);
+            $table->decimal('gold_price', 9, 2)->default(0.00);
+            $table->decimal('platinum_price', 9, 2)->default(0.00);
+            $table->integer('capacity');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
