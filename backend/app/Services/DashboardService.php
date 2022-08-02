bcf329a81e6fc2dd738836fa33dcf48035816d60
Arslan Afzal <arslanafzal794@gmail.com>
2022-08-02 14:12:28 +0500

    Added today bookings in dashboard page
    Added today bookings in dashboard page

--- backend/app/Services/DashboardService.php
+++ backend/app/Services/DashboardService.php
@@ -5,6 +5,7 @@ namespace App\Services;
 use App\Http\Requests\EventRequest;
 use App\Models\Event;
 use App\Models\Ticket;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 
@@ -16,6 +17,7 @@ class DashboardService
             'status' => true,
             'eventCount' => Event::get()->count(),
             'ticketCount' => Ticket::get()->count(),
+            'todayBookings' => Ticket::whereDate('created_at', Carbon::today())->get()->count(),
         ]);
     }
 }
--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,12 +1,27 @@
 <template>
     <div class="bg-white pb-5 inner-wrapper">
         <div class="row">
-            <div class="col-md-6">
-                <div class="card-box" data-aos="fade-in" data-aos-easing="linear" data-aos-duration="1000">
+
+            <div class="col-md-4">
+                <div class="card-box one" data-aos="fade-in" data-aos-easing="linear" data-aos-duration="1000">
                     <div class="name">
                         <font-awesome-icon icon="fa-solid fa-calendar-days" class="text-muted" />
                         <span class="ps-1 text-muted">
-                            Events
+                            Today Bookings
+                        </span>
+                    </div>
+                    <div class="number">
+                        {{ todayBookings }}
+                    </div>
+
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card-box two" data-aos="fade-in" data-aos-easing="linear" data-aos-duration="1000">
+                    <div class="name">
+                        <font-awesome-icon icon="fa-solid fa-calendar-days" class="text-muted" />
+                        <span class="ps-1 text-muted">
+                            Total Events
                         </span>
                     </div>
                     <div class="number">
@@ -16,12 +31,12 @@
                 </div>
             </div>
 
-            <div class="col-md-6">
-                <div class="card-box" data-aos="fade-in" data-aos-easing="linear" data-aos-duration="1000">
+            <div class="col-md-4">
+                <div class="card-box three" data-aos="fade-in" data-aos-easing="linear" data-aos-duration="1000">
 
                     <div class="name">
                         <font-awesome-icon icon="fa-solid fa-ticket" class="text-muted" /> <span
-                            class="ps-1 text-muted">Tickets</span>
+                            class="ps-1 text-muted">Total Bookings</span>
                     </div>
                     <div class="number">
                         {{ ticketCount }}
@@ -39,7 +54,8 @@ export default {
     data() {
         return {
             eventCount: '',
-            ticketCount: ''
+            ticketCount: '',
+            todayBookings: '',
         }
     },
     mounted() {
@@ -57,6 +73,7 @@ export default {
             });
             this.eventCount = res.data.eventCount;
             this.ticketCount = res.data.ticketCount;
+            this.todayBookings = res.data.todayBookings;
 
         },
     },
@@ -71,11 +88,25 @@ $yellow : #F4BF01;
 }
 
 .card-box {
-    border-top: 6px $yellow solid;
+
     box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.1);
     border-radius: 7px;
     padding: 25px 20px;
     text-align: center;
+
+    &.one {
+        border-top: 6px #28A745 solid;
+    }
+
+    &.two {
+        border-top: 6px #17A2B8 solid;
+    }
+
+    &.three {
+
+        border-top: 6px#DC3545 solid;
+
+    }
 
     .name {
         font-size: 20px;
